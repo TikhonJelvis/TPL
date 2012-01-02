ae618734507f4ea261d044b433a85b28c79279bd
Tikhon Jelvis <tikhon@berkeley.edu>
2012-01-01 23:09:54 -0800

    Moved the TPLValue data type out into its own module.

--- src/TPL/Parse.hs
+++ src/TPL/Parse.hs
@@ -1,39 +1,8 @@
 module TPL.Parse (TPLValue (..), expressions) where
 
 import Control.Applicative ((<$>))
-import Data.List
 import Text.ParserCombinators.Parsec
-
-data TPLValue = Null
-              | Id String
-              | Number Int
-              | String String
-              | Boolean Bool
-              | Operator String
-              | List [TPLValue]
-              | Expression [TPLValue]
-              | Sequence [TPLValue]
-              | Function [TPLValue] TPLValue
-              | Native String
-              | If TPLValue TPLValue TPLValue
-
-showSeq vals = intercalate " " $ map show vals
-
-instance Show TPLValue where
-  show (Null)                 = "null"
-  show (Id id)                = id
-  show (String str)           = show str
-  show (Number int)           = show int
-  show (Operator name)        = name
-  show (Boolean bool)         = if bool then "true" else "false"
-  show (List vals)            = show vals
-  show (Expression vals)      = showSeq vals
-  show (Sequence vals)        = unlines $ map show vals
-  show (Function params body) =
-    "λ " ++ showSeq params ++ " → {" ++ show body ++ "}"
-  show (If condition consequent alternate) = "{?if " ++ show condition ++
-                                             " then " ++ show consequent ++
-                                             " else " ++ show alternate ++ "?}"
+import TPL.Value
                                              
 whiteSpace :: CharParser st ()
 whiteSpace = spaces
