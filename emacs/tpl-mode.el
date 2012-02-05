721ec0010cca21d077a38331e234f23b562f5a6c
Tikhon Jelvis <tikhon@berkeley.edu>
2012-02-04 16:53:45 -0800

    Fixed how strings and complex function definitions are highlighted. Added ≠ as a symbol for /=. Started highlighting single-quote strings.

--- emacs/tpl-mode.el
+++ emacs/tpl-mode.el
@@ -5,6 +5,16 @@
 
 (require 'generic-x)
 
+(defvar tpl-mode-syntax-table
+  (let ((table (make-syntax-table)))
+    (modify-syntax-entry ?' "\"'" table)
+    (modify-syntax-entry ?\n "> b" table)
+    (modify-syntax-entry ?\" "\"'" table)
+    table))
+
+(defun init ()
+  (set-syntax-table tpl-mode-syntax-table))
+
 (define-generic-mode 'tpl-mode
   ;; comment-list
   nil
@@ -22,11 +32,13 @@
     "get"
     "precedence"
     "precedenceOf"
-    "load")
+    "load"
+    "require")
   ;; font-lock-list
   '(("\\b\\([0-9]+\\|null\\|true\\|false\\)\\b" . font-lock-constant-face)
-    ("^[ \t]*\\([_a-zA-Z0-9]+\\)[ \t]*:=" 1 'font-lock-variable-name-face)
-    ("^[ \t]*\\([_a-zA-Z0-9]+\\)\\([ \t]+[_a-zA-Z0-9]+\\)+[ \t]*:=" 1 'font-lock-variable-name-face)
+    ("^[ \t]*\\([_a-zA-Z0-9]+\\)[ \t]*:=" 1 'font-lock-function-name-face)
+    ("^[ \t]*\\([_a-zA-Z0-9]+\\)\\([ \t]+\\([_a-zA-Z0-9]+\\|\\[.*\\]\\)\\)+[ \t]*:=" 1 'font-lock-function-name-face)
+    ("[+-=*&^%#@!?/.|~<>:]+" . 'font-lock-type-face)
     ("\\(->\\)" 
      (1 (prog1 ()
           (compose-region (match-beginning 1) (match-end 1) ?→)
@@ -38,10 +50,13 @@
     ("\\(\\\\\\)" 
      (1 (prog1 ()
           (compose-region (match-beginning 1) (match-end 1) ?λ)
+          (put-text-property (match-beginning 1) (match-end 1) 'font-lock-face 'font-lock-builtin-face))))
+    ("\\(/=\\)" (1 (prog1 ()
+          (compose-region (match-beginning 1) (match-end 1) ?≠)
           (put-text-property (match-beginning 1) (match-end 1) 'font-lock-face 'font-lock-builtin-face)))))
   ;; auto-mode-alist
   '(".tpl\\'")
   ;; function-list
-  '())
+  '(init))
 
 (provide 'tpl-mode)
