50fd19b867b719b940a5893bd5d28245839f73bf
Tikhon Jelvis <tikhon@berkeley.edu>
2012-07-31 11:08:37 -0700

    Added some more basic env functions.

--- src/TPL/Env.hs
+++ src/TPL/Env.hs
@@ -1,7 +1,9 @@
 module TPL.Env where
 
+import Data.Functor ((<$>), (<$))
+import Data.IORef   (readIORef, writeIORef, modifyIORef)
 import qualified Data.Map as M
-import Data.Maybe (fromMaybe)
+import Data.Maybe   (fromMaybe)
 
 import TPL.Error
 import TPL.Value
@@ -9,9 +11,24 @@ import TPL.Value
 und :: String -> Either Error a
 und = Left . UndefinedVariable
 
-getLocal :: Env -> String -> Result
-getLocal env name = maybe (und name) Right $ M.lookup name env
+getEnv :: String -> Env -> Result
+getEnv name env = maybe (und name) Right $ M.lookup name env
 
-setLocal :: Env -> String -> Value -> Either Error Env
-setLocal env name val = maybe (und name) (const newEnv) $ M.lookup name env
+setEnv :: String -> Value -> Env -> Either Error Env
+setEnv name val env = maybe (und name) (const newEnv) $ M.lookup name env
   where newEnv = Right $ M.insert name val env
+        
+defineEnv :: String -> Value -> Env -> Env 
+defineEnv name val env = M.insert name val env
+        
+getEnvRef :: String -> EnvRef -> IO Result
+getEnvRef name (EnvRef ref) = getEnv name <$> readIORef ref
+
+setEnvRef :: String -> Value -> EnvRef -> IO Result
+setEnvRef name val (EnvRef ref) = do env <- readIORef ref
+                                     case setEnv name val env of
+                                       Left err -> return $ Left err
+                                       Right env' -> Right val <$ writeIORef ref env'
+                                       
+defineEnvRef :: String -> Value -> EnvRef  -> IO Value
+defineEnvRef name val (EnvRef ref) = val <$ (modifyIORef ref $ defineEnv name val)
