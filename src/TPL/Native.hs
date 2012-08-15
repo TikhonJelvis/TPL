cc61bbbe32aacfacdc5703d5747c2a18a20f3b8c
Tikhon Jelvis <tikhon@berkeley.edu>
2012-08-15 00:07:51 -0700

    Added a bunch of native functions.

--- src/TPL/Native.hs
+++ src/TPL/Native.hs
@@ -1,7 +1,42 @@
+{-# LANGUAGE TypeSynonymInstances, FlexibleInstances, IncoherentInstances #-}
 module TPL.Native where
 
-import TPL.Error
+import Control.Arrow (first, second)
+import Data.Functor  ((<$>))
+
 import TPL.Value
 
-curryOpr :: NativeOpr -> Term -> Result Value
-curryOpr (NativeOpr name n fn) arg = return . Native . NativeOpr name (n - 1) $ \ args -> fn (arg:args)
+class Extract a where extract :: Term -> a
+
+class Pack a where pack :: a -> Value
+
+instance Pack Number where pack = Number
+instance Extract Number where
+  extract (NumericLiteral n) = fromInteger n
+  extract _                  = error "extract: not a number!"
+  
+instance Pack Int where pack = Number . toInteger
+                           
+instance Pack String where pack = String
+instance Extract String where
+  extract (StringLiteral s) = s
+  extract _                 = error "extract: not a string!"
+
+instance Pack Bool where pack = Bool
+instance Extract Bool where
+  extract (BoolLiteral b) = b
+  extract _               = error "extract: not a bool!"
+                         
+instance Pack a => Pack [a] where pack = List . map pack
+
+instance (Extract a, Pack b) => Pack (a -> b) where
+  pack f = Native . NativeOpr $ \ x -> return . pack . f $ extract x
+
+natives :: [(Value, Value)]
+natives = first Symbol <$> (convert math ++ convert comp)
+  where convert :: Pack a => [(String, a)] -> [(String, Value)]
+        convert = (second pack <$>)
+        math :: [(String, Integer -> Integer -> Integer)]
+        math = [("+", (+)), ("-", (-)), ("*", (*)), ("/", div)]
+        comp :: [(String, Integer -> Integer -> Bool)]
+        comp = [(">", (>)), ("<", (<)), ("<=", (<=)), (">=", (>=))]
