b7fbd7168d263ac55eae3f9cecd66041d2e444b3
Tikhon Jelvis <tikhon@berkeley.edu>
2012-09-18 22:22:17 -0700

    Fixed the unification to work properly with the new operator precedence resolution.

--- src/TPL/Pattern.hs
+++ src/TPL/Pattern.hs
@@ -1,23 +1,24 @@
 module TPL.Pattern (unify) where
 
-import TPL.Value
+import           TPL.Value
 
 unify :: Term -> Value -> [(String, Value)]
-unify (Id name) val = [(name, val)]
+unify (Expression [term]) val    = unify term val
+unify (Id name) val              = [(name, val)]
 unify (ListLiteral ls) (List vs) = case last ls of
-  Lambda [_] (Expression (pat : Operator "..." : _)) ->
+  Expression [pat, Operator "..."] ->
     unifyMany (init ls) (take len vs) ++ unifyRest pat (drop len vs)
   _ -> unifyMany ls vs
-  where len = length $ init ls
-unify (ls@ListLiteral{}) val = unify ls $ List [val]
-unify (Lambda _ pat) val = unify pat val
-unify pat _ = error $ "Invalid pattern: " ++ show pat
+  where len                      = length $ init ls
+unify (ls@ListLiteral{}) val     = unify ls $ List [val]
+unify (Lambda _ pat) val         = unify pat val
+unify pat _                      = error $ "Invalid pattern: " ++ show pat
 
 unifyMany :: [Term] -> [Value] -> [(String, Value)]
 unifyMany pats vals = zip pats (vals ++ repeat Null) >>= uncurry unify
 
 unifyRest :: Term -> [Value] -> [(String, Value)]
-unifyRest pat []   = unify pat Null
+unifyRest pat []       = unify pat Null
 unifyRest pat restVals = combine $ restVals >>= unify pat
   where combine ((name, val):rest) = (name, List $ val : [v | (n, v) <- rest, n == name]) :
                              (combine $ filter ((/= name) . fst) rest)
