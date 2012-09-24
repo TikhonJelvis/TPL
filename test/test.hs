82452e77290e9542b4127f24729c64851d4532d9
Tikhon Jelvis <tikhon@berkeley.edu>
2012-09-24 11:32:28 -0700

    Got the tests to build. I still can't figure out how to run them though.

--- test/test.hs
+++ test/test.hs
@@ -1,13 +1,33 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Main where
 
-import Test.QuickCheck
-import Test.QuickCheck.All
+import           Control.Monad
 
-import TPL.Test.Parse
+import           Data.Functor
+import           Data.List
 
-main = $quickCheckAll
+import           System.Directory
+import           System.Exit
+import           System.IO.Silently
 
-tests = [prop_parseNull, prop_parseId, prop_parseNum, prop_parseStr, 
-         prop_parseBool, prop_parseOp, prop_parseBlock, prop_parseIf,
-         prop_parseIfElse]
+import           Test.QuickCheck
+import           Test.QuickCheck.All
+
+import           TPL.Run
+
+main = do samples <- runSamples
+          if samples then runQuickCheck else do putStr "FAILED"
+                                                exitFailure
+
+runSamples = do files <- getCurrentDirectory >>= getDirectoryContents
+                let scripts = sort $ filter (isSuffixOf ".tpl") files
+                let out     = sort $ filter (isSuffixOf ".out") files
+                and <$> zipWithM runTest scripts out
+  where runTest file out = do (result, ()) <- capture $ runFile file
+                              return $ result == out
+
+runQuickCheck = return () -- $quickCheckAll
+
+-- tests = [prop_parseNull, prop_parseId, prop_parseNum, prop_parseStr,
+--          prop_parseBool, prop_parseOp, prop_parseBlock, prop_parseIf,
+--          prop_parseIfElse]
