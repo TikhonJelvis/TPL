aea122681a0f46e278a8341b3beb2cf7fa484c75
Tikhon Jelvis <tikhon@berkeley.edu>
2012-09-25 18:25:35 -0700

    A pattern like xs... now defaults to [] rather than null.

--- src/TPL/Pattern.hs
+++ src/TPL/Pattern.hs
@@ -18,7 +18,7 @@ unifyMany :: [Term] -> [Value] -> [(String, Value)]
 unifyMany pats vals = zip pats (vals ++ repeat Null) >>= uncurry unify
 
 unifyRest :: Term -> [Value] -> [(String, Value)]
-unifyRest pat []       = unify pat Null
+unifyRest pat []       = unify pat $ List []
 unifyRest pat restVals = combine $ restVals >>= unify pat
   where combine ((name, val):rest) = (name, List $ val : [v | (n, v) <- rest, n == name]) :
                              (combine $ filter ((/= name) . fst) rest)
